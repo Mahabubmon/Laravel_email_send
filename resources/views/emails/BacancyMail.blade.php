94d01451b2d18deb9ff341c433cb8c2f4c22abfa
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-12 00:40:48 +0600

    Bacancy_mail

--- resources/views/emails/BacancyMail.blade.php
+++ resources/views/emails/BacancyMail.blade.php
@@ -1,12 +1,18 @@
-<x-mail::message>
-# Introduction
+@component('mail::message')
+<h2>Hello {{$body['name']}},</h2>
+<p>The email is a sample email for Laravel Tutorial: How to Send an Email using Laravel 8 from
+    @component('mail::button', ['url' => $body['url_a']])
+    Bacancy Technology
+    @endcomponent</p>
 
-The body of your message.
+<p>Visit @component('mail::button', ['url' => $body['url_b']])
+    Laravel Tutorials
+    @endcomponent and learn more about the Laravel framework.</p>
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+
+Happy coding!<br>
 
 Thanks,<br>
-{{ config('app.name') }}
-</x-mail::message>
+{{ config('app.name') }}<br>
+Laravel Team.
+@endcomponent
